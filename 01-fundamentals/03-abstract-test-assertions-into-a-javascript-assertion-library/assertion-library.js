1907737f7d805daf6a63aa489c7a5a1d01c58b86
Max Yokha <max.yokha@gmail.com>
2021-01-24 16:53:48 +0200

    Lesson 3 final: implement trivial assertion fn

--- 01-fundamentals/03-abstract-test-assertions-into-a-javascript-assertion-library/assertion-library.js
+++ 01-fundamentals/03-abstract-test-assertions-into-a-javascript-assertion-library/assertion-library.js
@@ -4,12 +4,18 @@ let result, expected
 
 result = sum(3, 7)
 expected = 10
-
-if (result !== expected)
-  throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expect = 4
+expect(result).toBe(expected)
 
-if (result !== expected)
-  throw new Error(`${result} is not equal to ${expected}`)
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+  }
+}
